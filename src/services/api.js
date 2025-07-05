9441dbbf3a7a4587842f1da13d3450eaa58ced07
NadavNV <nadav.nevo@gmail.com>
2025-07-05 12:01:37 +0300

    Fixed baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5200",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://smart-home-backend-svc.smart-home:5200",
 });
 
 export const getDeviceIds = async () => {
